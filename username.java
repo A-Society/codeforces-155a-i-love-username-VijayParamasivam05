5ae26e33462f636f0b44180c1b92f33eca2ab0ed
Vijay <vijayp2019@srishakthi.ac.in>
2018-07-25 18:31:27 +0530

    :fire: Remove Enter n prompt

--- username.java
+++ username.java
@@ -4,7 +4,6 @@ public class Main
 {
 public static void main(String[] args)throws Exception {
     BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-    System.out.println("Enter n:");
     int n=Integer.parseInt(br.readLine());
     int a[]=new int[n];
     for(int i=0;i<n;i++)
